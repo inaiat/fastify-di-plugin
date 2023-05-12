ac77e32afd7cd601fb512f6ac7403f5c20d4d08f
Inaiat <inaiat@gmail.com>
2023-05-12 15:27:40 -0300

    feat: distribute esm and cjs package

--- tests/simple.test.ts
+++ tests/simple.test.ts
@@ -4,69 +4,69 @@ import {FastifyDiAwilix} from '../src/fastify-di-plugin.js'
 import {getConfiguredTestServer} from './helpers/helpers.js'
 
 declare module '../src/index' {
-	interface Cradle {
-		dateService: Date;
-		printDate: string;
-	}
+  interface Cradle {
+    dateService: Date;
+    printDate: string;
+  }
 }
 
 test('shoud d.i. and use printService on request', async t => {
-	const dateService = () => new Date()
-	const printService = ({dateService: Date}) => dateService().toDateString()
+  const myDateService = () => new Date()
+  const printService = ({dateService: Date}) => myDateService().toDateString()
 
-	const {server} = getConfiguredTestServer()
+  const {server} = getConfiguredTestServer()
 
-	const container = new FastifyDiAwilix(server)
+  const container = new FastifyDiAwilix(server)
 
-	container.register({
-		dateService: asFunction(dateService).singleton(),
-		printDate: asFunction(printService).singleton(),
-	})
+  container.register({
+    dateService: asFunction(myDateService).singleton(),
+    printDate: asFunction(printService).singleton(),
+  })
 
-	server.get(
-		'/status',
-		async request => {
-			const {cradle} = request.diScope
-			return cradle.printDate
-		},
-	)
+  server.get(
+    '/status',
+    async request => {
+      const {cradle} = request.diScope
+      return cradle.printDate
+    },
+  )
 
-	const login = await server.inject({
-		method: 'GET',
-		url: '/status',
-	})
+  const login = await server.inject({
+    method: 'GET',
+    url: '/status',
+  })
 
-	t.is(server.diContainer.options.injectionMode, InjectionMode.PROXY)
-	t.is(login.statusCode, 200)
+  t.is(server.diContainer.options.injectionMode, InjectionMode.PROXY)
+  t.is(login.statusCode, 200)
 })
 
 test('shoud d.i. di', async t => {
-	const dateService = () => new Date()
-	const printService = ({dateService: Date}) => dateService().toDateString()
+  const dateService = () => new Date()
+  const printService = ({dateService: Date}) => dateService().toDateString()
 
-	const {server} = getConfiguredTestServer()
+  const {server} = getConfiguredTestServer()
 
-	const container = new FastifyDiAwilix(server)
+  const container = new FastifyDiAwilix(server)
 
-	container.register({
-		dateService: asFunction(dateService).singleton(),
-		printDate: asFunction(printService).singleton(),
-	})
+  container.register({
+    dateService: asFunction(dateService).singleton(),
+    printDate: asFunction(printService).singleton(),
+  })
 
-	server.get(
-		'/status',
-		async request => {
-			const {cradle} = request.diScope
-			return cradle.printDate
-		},
-	)
+  server.get(
+    '/status',
+    async request => {
+      const {cradle} = request.diScope
+      return cradle.printDate
+    },
+  )
 
-	const login = await server.inject({
-		method: 'GET',
-		url: '/status',
-	})
+  const login = await server.inject({
+    method: 'GET',
+    url: '/status',
+  })
 
-	t.is(server.diContainer.options.injectionMode, InjectionMode.PROXY)
-	t.is(login.statusCode, 200)
+  t.is(server.diContainer.options.injectionMode, InjectionMode.PROXY)
+  t.is(login.statusCode, 200)
 })
 
